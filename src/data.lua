2caba9bf8c03a42c1b202031972c2ebb3d0bdd1a
Allan Wegan <allanwegan@allanwegan.de>
2018-07-11 20:45:12 +0200

    Version 0.0.1

---
+++ src/data.lua
@@ -0,0 +1,5 @@
+--[[
+This file is part of the mod ConfigLampTimes that is licensed under the
+GNU GPL-3.0 or later. See the file COPYING for a copy of the GNU GPLv3.0.
+]]
+data.raw["item"]["battery"].icon = "__CleanBatteryIcon__/battery.png"
